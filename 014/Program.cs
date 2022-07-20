957c8dd5151bc5ac392aa759d5866d091fe4d6a2
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-20 21:23:04 +0300

    014

---
+++ 014/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число"); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число"); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+if (a % b == 0)
+{
+   Console.WriteLine($"{a} кратно {b}");  
+}
+else Console.WriteLine(a % b);
