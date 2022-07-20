322ec94f7c276dd43305e1433170b29a75a22ace
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-20 18:45:55 +0300

    002

---
+++ 002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a*a == b) 
+{
+    Console.WriteLine("b является квадратом a");
+}
+else if (b*b == a) 
+{
+     Console.WriteLine("a является квадратом b");
+}
+else Console.WriteLine("Число не являются квадратом другого");
