0271d781764e4abf3c31310b8612d8d7db13fec7
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-20 19:27:34 +0300

    005

---
+++ 005/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c || a==b)
+{
+    Console.WriteLine($"{a}");
+}
+if (b > a && b > c || b ==c)
+{
+    Console.WriteLine($"{b}");
+}
+if (c > a && c > b || c == a)
+{
+    Console.WriteLine($"{a}");
+}
+if (c == a && c == b)
+{
+    Console.WriteLine("Числа равны");
+}
