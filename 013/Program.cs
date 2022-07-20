b9b9e0fd9c70de69b05afd7c944263f69dafb0f4
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-20 21:14:54 +0300

    013

---
+++ 013/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число"); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+if (a <= 9)
+{
+    Console.WriteLine("Введите число больше 9"); 
+}
+else if (a >= 10 && a < 100)
+{
+    Console.WriteLine(a / 10); 
+}
+else if (a >= 100 && a < 1000)
+{
+    int b = a / 100;
+    int c = a % 10;
+    Console.WriteLine($"{b}{c}"); 
+}
