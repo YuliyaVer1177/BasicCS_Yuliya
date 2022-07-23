4e1ca21fc849920b42ee5b87b05ef12a25af23be
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-23 16:14:23 +0300

    012

--- 012/Program.cs
+++ 012/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите число"); 
+﻿//У целого числа из диапазона [10, 99] показать наибольшую цифру числа
+Console.WriteLine("Введите число"); 
 int a = Convert.ToInt32(Console.ReadLine()); 
 int b = a % 10;
 int c = a / 10;
