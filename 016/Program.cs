92a2a3676a227f86f883af0800e3964eff6c5c8e
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-23 16:15:57 +0300

    016

---
+++ 016/Program.cs
@@ -0,0 +1,9 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+Console.WriteLine("Введите число"); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+bool b = true;
+if (a % 7 == 0 && a % 23 == 0)
+{
+    System.Console.WriteLine(b);
+}
+else System.Console.WriteLine(!b);
