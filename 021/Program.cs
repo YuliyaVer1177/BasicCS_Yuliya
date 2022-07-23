35c56abd5e7de92a4e5a31f89daeb78af3f6bedb
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-23 16:17:04 +0300

    021

---
+++ 021/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Введите номер четверти"); 
+int part = Convert.ToInt32(Console.ReadLine());
+if (part == 1)
+{
+    System.Console.WriteLine("x > 0 , y > 0");
+}
+else if (part == 2)
+{
+    System.Console.WriteLine("x < 0 , y > 0");
+}
+else if (part == 3)
+{
+    System.Console.WriteLine("x < 0 , y < 0");
+}
+else if (part == 4)
+{
+    System.Console.WriteLine("x > 0 , y < 0");
+}
+else System.Console.WriteLine("Введите от 1 до 4");
