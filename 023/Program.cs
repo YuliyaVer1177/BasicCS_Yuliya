109b07ee0a97f7e6de6a976d3580550a3b420549
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-23 16:17:39 +0300

    023

---
+++ 023/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти расстояние между точками в пространстве 2D
+Console.WriteLine("Введите x1"); 
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y1"); 
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите x2"); 
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y2"); 
+int y2 = Convert.ToInt32(Console.ReadLine());
+double Distanse  (int x1, int x2, int y1, int y2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+}
+System.Console.WriteLine(Distanse(x1, y1, x2, y2));
