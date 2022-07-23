6987e15f81f2c6ff4c325bff5f086dcea0734a17
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-23 16:12:46 +0300

    009

--- 009/Program.cs
+++ 009/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите число");
+﻿//Вывести на экран четные числа от1 до N
+Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
 int i = 2;
 while (i <= a)
