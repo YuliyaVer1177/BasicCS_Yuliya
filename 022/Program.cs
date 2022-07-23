b2d96cc5bea4454ab2f177f96500ce317349ea96
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-23 16:17:26 +0300

    022

---
+++ 022/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа проверяет пятизначное число на палиндромом
+Console.WriteLine("Введите число"); 
+int a = Convert.ToInt32(Console.ReadLine());
+bool checkout (int a)
+{
+ int a1 = a / 10000;
+ int a2 = a / 1000 % 10;
+ int a3 = a % 100 / 10;
+ int a4 = a %  10;
+ return a1==a4 && a2==a3;
+}
+System.Console.WriteLine(checkout (a));
