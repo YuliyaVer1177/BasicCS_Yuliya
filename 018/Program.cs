1eebd5fe90af7e3ee289a5e335cc5ad726e28138
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-23 16:16:25 +0300

    018

---
+++ 018/Program.cs
@@ -0,0 +1,10 @@
+﻿//По двум заданным числам проверять яв-ся ли одно квадратом другого. Исп Подпрограмму
+Console.WriteLine("Введите число"); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число"); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+bool checkout (int a, int b)
+{
+    return a == b*b || b == a*a;
+}
+System.Console.WriteLine(checkout(a, b));
