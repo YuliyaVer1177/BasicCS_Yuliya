79a3f31cdc3dbb2737bf909699e43262f798b149
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-23 16:08:29 +0300

    000

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Введите число:");
+﻿//Вводится целое число. Вывести квадрат числа
+Console.WriteLine("Введите число:");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = a*a;
-Console.Write("a*a = ");
 Console.WriteLine(b);
