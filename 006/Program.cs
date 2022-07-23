1cfd31d29dd37a5fff6030cef1d2e6b2f2f5ff57
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-23 16:11:36 +0300

    006

--- 006/Program.cs
+++ 006/Program.cs
@@ -1,5 +1,16 @@
-﻿Console.WriteLine("Введите число");
+﻿//6. написать программу вычисления значения функций y = sin(a)
+
+/*Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
 double y;
 y = Math.Sin(a);
-Console.WriteLine(y);
+Console.WriteLine(y);*/
+
+double x=Math.PI;
+double y=Math.Sin(x);
+double y1=Math.Sin(x*2);
+System.Console.WriteLine($"{y:G}");
+System.Console.WriteLine(9.0/10000000.0);
+
+if (Math.Abs(y-y1)<= 1E-10) System.Console.WriteLine("ОК"); // сравнение двух дробных чисел с погрешностью
+else System.Console.WriteLine("Wrong");
