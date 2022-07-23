c1dc922f19d62b26e73aaf22b254a51ac291151b
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-23 16:13:50 +0300

    010

--- 010/Program.cs
+++ 010/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите число"); 
+﻿//Вывести на экран последнюю цифру целого числа введенного с клавиатуры
+Console.WriteLine("Введите число"); 
 int a = Convert.ToInt32(Console.ReadLine());
 int b = a % 10; //b = остатку от деления a на 10 
 Console.WriteLine(b);
