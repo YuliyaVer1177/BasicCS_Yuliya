8a1393f4bee3e5a27cf45a0e5bf346b51378e533
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-23 16:16:09 +0300

    017

---
+++ 017/Program.cs
@@ -0,0 +1,9 @@
+﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Введите число"); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+bool x = true;
+if (a == 6 || a == 7)
+{
+    System.Console.WriteLine(x);
+}
+else System.Console.WriteLine(!x);
