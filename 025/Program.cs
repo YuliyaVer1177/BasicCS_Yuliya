fe213e384afa03e6cd06369acec9f8afc3963116
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-25 20:12:02 +0300

    025

---
+++ 025/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывести на экран таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите число"); 
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+System.Console.Write("number | " );
+System.Console.WriteLine("  cube" );
+System.Console.WriteLine("_______________" );
+
+while (i <= a)
+{
+    //System.Console.WriteLine(i*i);
+    System.Console.Write($"{i}      |");
+    System.Console.WriteLine($" {Math.Pow(i,3)}" );
+    System.Console.WriteLine("_______________" );
+    i = i + 1;
+}
+
