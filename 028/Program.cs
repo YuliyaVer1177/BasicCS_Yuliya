15cd073e0f2ec1acfa17ac720c0ac6d03cecfcb3
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-25 20:12:46 +0300

    028

---
+++ 028/Program.cs
@@ -0,0 +1,19 @@
+﻿//Определить колич-во цифр в числе. Сделать подпрограмму
+Console.WriteLine("Введите число A = ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+int countDig(int a){ //подпрограмма в которой будет действие
+if (a == 0)
+{
+    return 1;
+
+}
+else while (a != 0)
+{
+    i = i+1;
+    a = a/10;
+}
+return i;
+}
+System.Console.WriteLine(countDig(a));
