542265f52d9863aa9d5e90ff38fa5119bdfabce3
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-25 20:12:58 +0300

    029

---
+++ 029/Program.cs
@@ -0,0 +1,17 @@
+﻿// Посчитать сумму цифр в числе
+Console.WriteLine("Введите число"); 
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+int sumDig(int a)
+{
+    while (a != 0)
+    {
+        int k = a%10;
+        sum = sum + k;
+        a = a/10;
+    }
+    return sum;
+}
+
+System.Console.WriteLine(sumDig(a));
