71a37c859820f6c7e5eae69011c9d5b8fa0ba70a
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-25 20:11:46 +0300

    024

---
+++ 024/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывести на экран таблицу квадратов чисел от 1 до N
+Console.WriteLine("Введите число"); 
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= a)
+{
+    //System.Console.WriteLine(i*i);
+    System.Console.Write($" {Math.Pow(i,2)}" );
+    i = i + 1;
+}
