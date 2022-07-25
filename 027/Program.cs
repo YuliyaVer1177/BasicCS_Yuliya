5ca8e7b800f5cd6b3f71e532abbfd4c09b11d293
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-25 20:12:32 +0300

    027

---
+++ 027/Program.cs
@@ -0,0 +1,13 @@
+﻿//Возведите число а в натуральную степень b используя цикл
+Console.WriteLine("Введите число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int pow = 1;
+while (i <= b)
+{
+    pow = pow*a;
+    i = i + 1;
+}
+System.Console.WriteLine(pow);
