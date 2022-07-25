880490eb6e17f4f8bdb2cbf3a483d231a533e74d
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-25 20:13:12 +0300

    030

---
+++ 030/Program.cs
@@ -0,0 +1,16 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число"); 
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int mult = 1;
+ 
+int Multiplication(int a)
+{
+    while (i <= a)
+    {
+        mult = mult*i;
+        i ++;
+    }
+    return mult;
+}
+System.Console.WriteLine(Multiplication(a));
