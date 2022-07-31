0044ff5a1d14f3b120acf0a2a425e567c7fc1a06
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-07-31 13:30:40 +0300

    015

--- 015/Program.cs
+++ 015/Program.cs
@@ -1,8 +1,19 @@
 ﻿//С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет.
-Console.WriteLine("Введите число"); 
+/*Console.WriteLine("Введите число"); 
 int a = Convert.ToInt32(Console.ReadLine()); 
 if (a < 100)
 {
    Console.WriteLine("Это число не имеет третьей цифры"); 
 }
-else Console.WriteLine(a.ToString()[2]);
+else Console.WriteLine(a.ToString()[2]);*/ //мое решение
+
+int a = Convert.ToInt32(Console.ReadLine()); //решение на семинаре функция по модулю Math.Abc
+//if (a >= 100 || a <= -100)
+if (Math.Abs(a)>=100)
+{
+   Console.WriteLine(a/100%10);
+}
+else 
+{
+   Console.WriteLine("Это число не имеет третьей цифры");
+}
